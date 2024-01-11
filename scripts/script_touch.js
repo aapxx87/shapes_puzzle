3076c6a5c3a1917aff76d9f6c76625eed6e8f235
aapxx87 <57785764+aapxx87@users.noreply.github.com>
2024-01-11 11:56:31 +0300

    new

--- scripts/script_touch.js
+++ scripts/script_touch.js
@@ -1,23 +1,20 @@
-function getPositionAndRotation(element) {
-  const style = window.getComputedStyle(element);
-  const matrix = new WebKitCSSMatrix(style.transform);
-  return {
-    top: element.offsetTop,
-    left: element.offsetLeft,
-    rotation_angle: Math.round(Math.atan2(matrix.m21, matrix.m11) * (180 / Math.PI))
-  };
-}
-
 function makeMovable(element, figureName) {
   let initialX = 0, initialY = 0;
-  let posX = figures[figureName].left, posY = figures[figureName].top;
 
-  // Синхронизация начального состояния с CSS
-  const initialState = getPositionAndRotation(element);
-  figures[figureName] = { ...initialState };
+  const updatePositionAndRotation = () => {
+    let posX = figures[figureName].left, posY = figures[figureName].top;
+    let rotation = figures[figureName].rotation_angle;
+
+    element.style.top = `${posY}px`;
+    element.style.left = `${posX}px`;
+    element.style.transform = `rotate(${rotation}deg)`;
+  };
 
   element.addEventListener('touchstart', (e) => {
     if (e.touches.length === 1) {
+      updatePositionAndRotation();
+
+      let posX = figures[figureName].left, posY = figures[figureName].top;
       initialX = e.touches[0].clientX - posX;
       initialY = e.touches[0].clientY - posY;
     }
@@ -28,18 +25,19 @@ function makeMovable(element, figureName) {
     if (e.touches.length === 1) {
       let currentX = e.touches[0].clientX;
       let currentY = e.touches[0].clientY;
-      posX = currentX - initialX;
-      posY = currentY - initialY;
+      let posX = currentX - initialX;
+      let posY = currentY - initialY;
+
       element.style.top = `${posY}px`;
       element.style.left = `${posX}px`;
-    }
 
-    figures[figureName].left = posX;
-    figures[figureName].top = posY;
+      figures[figureName].left = posX;
+      figures[figureName].top = posY;
+    }
   });
 }
 
-// Применение функции makeMovable к каждому элементу
+// Получение ссылок на элементы и применение функции makeMovable
 const elements = {
   'square': document.querySelector('.square'),
   'triangle_1_big': document.querySelector('.triangle_1_big'),
@@ -53,3 +51,63 @@ const elements = {
 Object.keys(elements).forEach(key => {
   makeMovable(elements[key], key);
 });
+
+
+
+
+
+
+
+
+
+// function makeMovable(element, figureName) {
+//   let posX = figures[figureName].left, posY = figures[figureName].top;
+//   let initialX = 0, initialY = 0;
+
+//   // Установка начального положения и угла поворота
+//   element.style.top = `${posY}px`;
+//   element.style.left = `${posX}px`;
+//   element.style.transform = `rotate(${figures[figureName].rotation_angle}deg)`;
+
+//   element.addEventListener('touchstart', (e) => {
+//     if (e.touches.length === 1) {
+//       initialX = e.touches[0].clientX - posX;
+//       initialY = e.touches[0].clientY - posY;
+//     }
+//   });
+
+//   element.addEventListener('touchmove', (e) => {
+//     e.preventDefault();
+//     if (e.touches.length === 1) {
+//       let currentX = e.touches[0].clientX;
+//       let currentY = e.touches[0].clientY;
+//       posX = currentX - initialX;
+//       posY = currentY - initialY;
+//       element.style.top = `${posY}px`;
+//       element.style.left = `${posX}px`;
+//     }
+
+//     figures[figureName].left = posX;
+//     figures[figureName].top = posY;
+//   });
+// }
+
+// // Применение функции makeMovable к каждому элементу
+// const elements = {
+//   'square': document.querySelector('.square'),
+//   'triangle_1_big': document.querySelector('.triangle_1_big'),
+//   'triangle_2_big': document.querySelector('.triangle_2_big'),
+//   'triangle_1_medium': document.querySelector('.triangle_1_medium'),
+//   'triangle_1_small': document.querySelector('.triangle_1_small'),
+//   'triangle_2_small': document.querySelector('.triangle_2_small'),
+//   'parallelepiped': document.querySelector('.parallelepiped')
+// };
+
+
+// Object.keys(elements).forEach(key => {
+//   makeMovable(elements[key], key);
+// });
+
+
+
+
